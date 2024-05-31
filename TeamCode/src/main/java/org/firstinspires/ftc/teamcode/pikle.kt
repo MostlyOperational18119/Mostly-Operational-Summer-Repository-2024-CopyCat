eb889497196e5575006f342394fc66fd5dc5b1ce
MostlyOperational18119 <64237204+MostlyOperational18119@users.noreply.github.com>
2024-05-31 16:06:41 -0700

    pikle 🥰

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pikle.kt
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp
+import com.qualcomm.robotcore.hardware.DcMotor
+
+@TeleOp(name = "pikle \uD83E\uDD70", group="pikle")
+class pikle: DriveMethods() {
+    @Override
+    override fun runOpMode() {
+        val motor1 = hardwareMap.get(DcMotor::class.java, "m1")
+        val motor2 = hardwareMap.get(DcMotor::class.java, "m2")
+        val motor3 = hardwareMap.get(DcMotor::class.java, "m3")
+        waitForStart()
+        while (opModeIsActive()) {
+            if (gamepad1.a) {
+                motor1.power = 1.0
+                motor2.power = 1.0
+                motor3.power = 1.0
+            }
+        }
+    }
+}
