847b89503a3273ef7565d3cfc25cda9d5ebef3ac
MostlyOperational18119 <64237204+MostlyOperational18119@users.noreply.github.com>
2024-05-31 16:09:53 -0700

    pikle 2.0 screw build heh...

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pikle.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pikle.kt
@@ -12,7 +12,7 @@ class pikle: DriveMethods() {
         val motor3 = hardwareMap.get(DcMotor::class.java, "m3")
         waitForStart()
         while (opModeIsActive()) {
-            if (gamepad1.a) {
+            if (gamepad1.a && gamepad1.x && gamepad1.left_stick_button) {
                 motor1.power = 1.0
                 motor2.power = 1.0
                 motor3.power = 1.0
