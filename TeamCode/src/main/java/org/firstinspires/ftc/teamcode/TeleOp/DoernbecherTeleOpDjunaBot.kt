065acd092255f29457fbf66fab5e38cab51b6f21
TheComputerToucher <damien.brown15@outlook.com>
2024-06-14 11:26:06 -0700

    Make some stuff readable, and work on tuning odometry

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/DoernbecherTeleOpDjunaBot.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/DoernbecherTeleOpDjunaBot.kt
@@ -10,9 +10,8 @@ import com.qualcomm.robotcore.hardware.Servo
 class DoernbecherTeleOpDjunaBot: LinearOpMode() {
     override fun runOpMode() {
         val motorFL = hardwareMap.get(DcMotor::class.java, "motorFL")
-        //reverse FR
-        val motorFR = hardwareMap.get(DcMotor::class.java, "motorFR")
-        motorFR.direction=DcMotorSimple.Direction.REVERSE;
+        val motorFR = hardwareMap.get(DcMotor::class.java, "motorFR") //reverse FR
+        motorFR.direction = DcMotorSimple.Direction.REVERSE
         val motorBL = hardwareMap.get(DcMotor::class.java, "motorBL")
         val motorBR = hardwareMap.get(DcMotor::class.java, "motorBR")
         val aimMotor = hardwareMap.get(DcMotor::class.java, "aimMotor")
@@ -42,15 +41,15 @@ class DoernbecherTeleOpDjunaBot: LinearOpMode() {
             telemetry.addLine("motorFL: ${motorFL.power} motorFR: ${motorFR.power} motorBL: ${motorBL.power} motorBR: ${motorBR.power} ")
             if(gamepad1.dpad_down && aimMotor.currentPosition<300){
                 telemetry.addLine("Running AIM Motor Original Position: ${aimMotor.currentPosition}");
-                aimMotor.power=0.1;
-                sleep(80);
-                aimMotor.power=0.0;
+                aimMotor.power = 0.1
+                sleep(80)
+                aimMotor.power = 0.0
                 telemetry.addLine("Running AIM Motor New Position: ${aimMotor.currentPosition}");
             }else if(gamepad1.dpad_up && aimMotor.currentPosition>-250){
                 telemetry.addLine("Running AIM Motor Original Position: ${aimMotor.currentPosition}");
-                aimMotor.power=-0.2;
-                sleep(80);
-                aimMotor.power=0.0;
+                aimMotor.power = -0.2
+                sleep(80)
+                aimMotor.power = 0.0
                 telemetry.addLine("Running AIM Motor New Position: ${aimMotor.currentPosition}");
             }
 
