c066b0f4d0f64565a240360e310cf7f053f21e97
MostlyOperational18119 <64237204+MostlyOperational18119@users.noreply.github.com>
2024-07-12 11:39:47 -0700

    ohio sigma gyatt

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/motorTester.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/motorTester.kt
@@ -10,7 +10,7 @@ class motorTester: LinearOpMode() {
         val motorFR = hardwareMap.get(DcMotor::class.java, "motorFR")
         val motorBL = hardwareMap.get(DcMotor::class.java, "motorBL")
         val motorBR = hardwareMap.get(DcMotor::class.java, "motorBR")
-        val aimMotor = hardwareMap.get(DcMotor::class.java, "aimMotor")
+        //val aimMotor = hardwareMap.get(DcMotor::class.java, "aimMotor")
         val speedDiv = 2.0
 
         telemetry.addData("Status", "Initialized")
@@ -27,21 +27,23 @@ class motorTester: LinearOpMode() {
             }else if(gamepad1.x){
                 telemetry.addLine("Running motorBR");
                 motorBR.power=1.0;
-            }else if(gamepad1.y){
+            }else if(gamepad1.y) {
                 telemetry.addLine("Running motorFR");
-                motorFR.power=1.0;
-            }else if(gamepad1.dpad_down){
-                telemetry.addLine("Running AIM Motor Original Position: ${aimMotor.currentPosition}");
-                aimMotor.power=0.1;
-                sleep(80);
-                aimMotor.power=0.0;
-                telemetry.addLine("Running AIM Motor New Position: ${aimMotor.currentPosition}");
-            }else{
+                motorFR.power = 1.0;
+            }
+//            }else if(gamepad1.dpad_down){
+//                telemetry.addLine("Running AIM Motor Original Position: ${aimMotor.currentPosition}");
+//                aimMotor.power=0.1;
+//                sleep(80);
+//                aimMotor.power=0.0;
+//                telemetry.addLine("Running AIM Motor New Position: ${aimMotor.currentPosition}");
+//            }
+            else{
                 motorBL.power=0.0;
                 motorBR.power=0.0;
                 motorFL.power = 0.0;
                 motorFR.power = 0.0;
-                aimMotor.power = 0.0;
+//                aimMotor.power = 0.0;
             }
             telemetry.update()
             sleep(10)
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -22,7 +22,7 @@ public class DriveConstants {
      * These are motor constants that should be listed online for your motors.
      */
     public static final double TICKS_PER_REV = 537.6;
-    public static final double MAX_RPM = 312;
+    public static final double MAX_RPM = 312.5;
 
     /*
      * Set RUN_USING_ENCODER to true to enable built-in hub velocity control using drive encoders.
@@ -32,7 +32,7 @@ public class DriveConstants {
      * If using the built-in motor velocity PID, update MOTOR_VELO_PID with the tuned coefficients
      * from DriveVelocityPIDTuner.
      */
-    public static final boolean RUN_USING_ENCODER = true;
+    public static final boolean RUN_USING_ENCODER = false;
     public static PIDFCoefficients MOTOR_VELO_PID = new PIDFCoefficients(0, 0, 0,
             getMotorVelocityF(MAX_RPM / 60 * TICKS_PER_REV));
 
@@ -65,7 +65,7 @@ public class DriveConstants {
      * small and gradually increase them later after everything is working. All distance units are
      * inches.
      */
-    public static double MAX_VEL = 50;
+    public static double MAX_VEL = 49.4736153753;
     public static double MAX_ACCEL = 50;
     public static double MAX_ANG_VEL = 3.125;
     public static double MAX_ANG_ACCEL = 1.5;
