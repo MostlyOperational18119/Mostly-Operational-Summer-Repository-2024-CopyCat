dbf79b9afa790daffc37d4875e209e1a8e889c8c
MostlyOperational18119 <64237204+MostlyOperational18119@users.noreply.github.com>
2024-07-19 09:30:13 -0700

    aaa

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/StandardTrackingWheelLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/StandardTrackingWheelLocalizer.java
@@ -31,11 +31,11 @@ public class StandardTrackingWheelLocalizer extends ThreeTrackingWheelLocalizer 
     public static double WHEEL_RADIUS = 0.945; // in
     public static double GEAR_RATIO = 1; // output (wheel) speed / input (encoder) speed
 
-    public static double LATERAL_DISTANCE = 11.9; // in; distance between the left and right wheels
-    public static double FORWARD_OFFSET = 1; // in; offset of the lateral wheel
+    public static double LATERAL_DISTANCE = 12.06; // in; distance between the left and right wheels
+    public static double FORWARD_OFFSET = -1; // in; offset of the lateral wheel
 
-    private static double X_MULTIPLIER = 0.98933714411; // 90 divided by 77.90211207450463
-    private static double Y_MULTIPLIER = 1.00480071452;
+    private static double X_MULTIPLIER = 1.003;
+    private static double Y_MULTIPLIER = 1.014;;
 
     private Encoder leftEncoder, rightEncoder, frontEncoder;
 
@@ -57,7 +57,8 @@ public class StandardTrackingWheelLocalizer extends ThreeTrackingWheelLocalizer 
 
         // TODO: reverse any encoders using Encoder.setDirection(Encoder.Direction.REVERSE)
 
-        rightEncoder.setDirection(Encoder.Direction.REVERSE);
+        leftEncoder.setDirection(Encoder.Direction.REVERSE);
+        frontEncoder.setDirection(Encoder.Direction.REVERSE);
     }
 
     public static double encoderTicksToInches(double ticks) {
