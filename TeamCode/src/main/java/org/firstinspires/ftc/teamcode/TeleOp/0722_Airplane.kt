1302727cdd5bdb68558c8662bbf07ddfcd5e8cd6
Gabriel N <g.p.nickerson@gmail.com>
2024-07-22 19:51:18 -0700

    Updated Airplane Launcher

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/0722_Airplane.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/0722_Airplane.kt
@@ -14,16 +14,17 @@ class `0722_Airplane`: LinearOpMode() {
         val motorBL = hardwareMap.get(DcMotor::class.java, "motorBL")
         val motorBR = hardwareMap.get(DcMotor::class.java, "motorBR")
         val airplaneServo = hardwareMap.get(Servo::class.java, "airplaneServo")
+        val launchMotor = hardwareMap.get(DcMotor::class.java, "aimMotor")
 
         motorFL.direction=DcMotorSimple.Direction.REVERSE;
         motorBL.direction=DcMotorSimple.Direction.REVERSE;
         motorBR.direction=DcMotorSimple.Direction.REVERSE;
+        launchMotor.mode=DcMotor.RunMode.STOP_AND_RESET_ENCODER;
+        launchMotor.mode=DcMotor.RunMode.RUN_USING_ENCODER;
 
         val lowerServoPosition = 0.65;
         val highestServoPosition = 0.95;
         val speedDiv = 2.0
-
-        var airplaneToggle=true;
 
         telemetry.addData("Status", "Initialized")
         telemetry.update()
@@ -42,18 +43,24 @@ class `0722_Airplane`: LinearOpMode() {
             telemetry.addData("Status", "Running")
 
 
-            if(gamepad1.a && airplaneToggle){
+            if(gamepad1.a){
                 airplaneServo.position=lowerServoPosition;
-                airplaneToggle = false;
                 sleep(1000)
-            }else if (gamepad1.a && !airplaneToggle) {
+            }else if (gamepad1.b) {
                 airplaneServo.position=highestServoPosition;
-                airplaneToggle = true;
                 sleep(1000)
             }
-
-            telemetry.update()
-            sleep(10)
+            if(gamepad1.dpad_up && launchMotor.currentPosition<=250) {
+                launchMotor.power=.07;
+            }
+            else if (gamepad1.dpad_down && launchMotor.currentPosition>=0) {
+                launchMotor.power=-0.07;
+            }
+            else {
+                launchMotor.power=0.0;
+            }
+            telemetry.addData("Rotate Pos", launchMotor.currentPosition);
+            telemetry.update();
         }
     }
 }
